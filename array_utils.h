83e7312c4a05e6ea7e4ce3baa9856e91f0fc549e
ahmadali <MY_NAME@example.com>
2023-12-21 10:12:02 -0800

    changes

--- array_utils.h
+++ array_utils.h
@@ -1,3 +1,12 @@
 
 
 //place your documentation and prototypes here
+/* Q1 : */int contains();
+
+/* Q2 : */int containsWithin();
+
+/* Q4 : */void reverse();
+
+/* Q3 : */int *paddedCopy();
+
+/* Q5 : */int * reverseCopy();
